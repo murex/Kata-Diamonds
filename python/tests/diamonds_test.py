18fdfd1a7059d8aecc3ed32ddd682df4b6c1ef39
Damien Menanteau <damien.menanteau@gmail.com>
2023-03-16 18:37:16 +0100

    Add python language

---
+++ python/tests/diamonds_test.py
@@ -0,0 +1,68 @@
+import unittest
+
+from diamonds.diamonds import draw
+
+
+class DiamondsTest(unittest.TestCase):
+
+    @unittest.skip("test currently disabled")  # Comment or remove this line to enable this test case
+    def test_acceptance_test(self):
+        letter = 'Z'
+        expected = [
+            '                         A                         \n',
+            '                        B B                        \n',
+            '                       C   C                       \n',
+            '                      D     D                      \n',
+            '                     E       E                     \n',
+            '                    F         F                    \n',
+            '                   G           G                   \n',
+            '                  H             H                  \n',
+            '                 I               I                 \n',
+            '                J                 J                \n',
+            '               K                   K               \n',
+            '              L                     L              \n',
+            '             M                       M             \n',
+            '            N                         N            \n',
+            '           O                           O           \n',
+            '          P                             P          \n',
+            '         Q                               Q         \n',
+            '        R                                 R        \n',
+            '       S                                   S       \n',
+            '      T                                     T      \n',
+            '     U                                       U     \n',
+            '    V                                         V    \n',
+            '   W                                           W   \n',
+            '  X                                             X  \n',
+            ' Y                                               Y \n',
+            'Z                                                 Z\n',
+            ' Y                                               Y \n',
+            '  X                                             X  \n',
+            '   W                                           W   \n',
+            '    V                                         V    \n',
+            '     U                                       U     \n',
+            '      T                                     T      \n',
+            '       S                                   S       \n',
+            '        R                                 R        \n',
+            '         Q                               Q         \n',
+            '          P                             P          \n',
+            '           O                           O           \n',
+            '            N                         N            \n',
+            '             M                       M             \n',
+            '              L                     L              \n',
+            '               K                   K               \n',
+            '                J                 J                \n',
+            '                 I               I                 \n',
+            '                  H             H                  \n',
+            '                   G           G                   \n',
+            '                    F         F                    \n',
+            '                     E       E                     \n',
+            '                      D     D                      \n',
+            '                       C   C                       \n',
+            '                        B B                        \n',
+            '                         A                         \n',
+        ]
+        self.assertEqual(''.join(expected), draw(letter))
+
+
+if __name__ == "__main__":
+    unittest.main()
